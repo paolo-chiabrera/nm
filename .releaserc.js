36751a9fb91efff42a7090cf959c399d0cfa44f3
Paolo Chiabrera <paolo.chiabrera@gmail.com>
2018-01-12 20:34:06 +0100

    fix: remove commitizen

--- .releaserc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "release": {
-    "verifyConditions": ["@semantic-release/changelog", "@semantic-release/npm", "@semantic-release/git", "@semantic-release/github"],
-    "getLastRelease": "@semantic-release/npm",
-    "publish": ["@semantic-release/changelog", "@semantic-release/npm", "@semantic-release/git", "@semantic-release/github"]
-  }
-};
